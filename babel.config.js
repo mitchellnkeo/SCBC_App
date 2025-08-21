509ab72736f87efc82ceaaf95f35ea094a868e28
mitchellnkeo <mitchellnkeo@gmail.com>
2025-08-20 20:49:32 -0700

    Created babel folder

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = function(api) {
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'],
+      plugins: [
+        'react-native-worklets/plugin'  // Changed from 'react-native-reanimated/plugin'
+      ],
+    };
+  };
